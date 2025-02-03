15eef0dcde47ffbd82b1ac0f7811dbed6521d505
Charan Arivarasan(UST <286178@ust.com>
2025-02-03 16:12:17 +0530

    made changes in requirements.txt file

--- frontend/src/features/ImageUploader.jsx
+++ frontend/src/features/ImageUploader.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState,  useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { motion, AnimatePresence } from "framer-motion";
